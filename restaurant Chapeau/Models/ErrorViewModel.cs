7af441b14106c4b4e712d7d59f8fd4b248a89ef3
Beheerder <Beheerder@DESKTOP-EB04OI0>
2025-05-14 23:13:39 +0200

    Add project files.

---
+++ restaurant Chapeau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace restaurant_Chapeau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
