ed532d08ea599280ed84c7261b60448d9a02a4b7
elcoleta <723295@student.inholland.nl>
2025-05-17 23:28:02 +0200

    Adding stock model

---
+++ restaurant Chapeau/Models/Stock.cs
@@ -0,0 +1,12 @@
+﻿namespace restaurant_Chapeau.Models
+{
+    public class Stock
+    {
+        public int StockID { get; set; }
+        public int MenuItemID { get; set; }
+        public int QuantityAvailable { get; set; }
+
+        public string delete;
+
+    }
+}
